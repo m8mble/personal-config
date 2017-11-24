76260091005a009b355117ffe45a93b2e6fd75f8
m8mble <m8mble@vivaldi.net>
2017-11-24 09:21:46 +0100

    Add setup.py stub with known todos

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+def _main():
+    # TODO: 
+    # -- xdg config: link config to ~/.config/user-dirs.dirs and call xdg-user-dirs-update
+    # -- powerline: 
+    #    -- pip install --user git+git://github.com/powerline/powerline
+    #    -- remainder of https://askubuntu.com/questions/283908/how-can-i-install-and-use-powerline-plugin
+    #    -- config
+    # -- vim:
+    #    -- get Vundle
+    #    -- link vimrc
+    #    -- install plugins
+    # -- bashrc
+    # -- .profile
+    print 'Nothing to do.'
+
+if __name__ == '__main__':
+    _main()
+
